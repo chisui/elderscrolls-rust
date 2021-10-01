4de86626b83796895833d7d32da87f7ede4d1ae5
Philipp Dargel <chisui.pd@gmail.com>
2021-10-01 18:20:29 +0200

    use niv

--- shell.nix
+++ shell.nix
@@ -1,17 +1,17 @@
-{ pkgs ? import <nixpkgs> {
-    overlays = [
-       (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
-    ];
-  }
-}:
-with pkgs;
+{ sources ? import ./nix/sources.nix }:
+let pkgs = import sources.nixpkgs {
+  overlays = [
+    (import sources.nixpkgs-mozilla)
+  ];
+};
+in with pkgs;
 let
   rust-channel = rustChannelOf {
     date = "2021-08-15";
     channel = "nightly";
   };
 in mkShell {
-  name = "bsa";
+  name = "shell";
   buildInputs = [
     (rust-channel.rust.override {
       extensions = [
@@ -21,4 +21,3 @@ in mkShell {
     })
   ];
 }
-
