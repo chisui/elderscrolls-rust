4168937b0733db35bc323290830a53622c5d660f
Philipp Dargel <chisui.pd@gmail.com>
2021-10-02 19:32:14 +0200

    refactored str errors for better testing

--- bsa/src/str.rs
+++ bsa/src/str.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{Read, Write, Seek, SeekFrom, Result, Error, ErrorKind},
+    io::{self, Read, Write, Seek, SeekFrom},
     str::{self, FromStr},
     convert::TryFrom,
 };
@@ -10,27 +10,36 @@ use newtype_derive_2018::*;
 use super::bin::{read_struct, Readable, Writable, write_many};
 
 #[derive(Debug, Error)]
-#[error("BSA Strings may only be {0} chars or less long since their length is stored in a byte")]
-pub struct StringToLong(usize);
+pub enum StrError {
+    #[error("string may only be {0} chars or less long since their length is stored in a byte")]
+    TooLong(usize),
+    #[error("{0}")]
+    Utf8Error(#[from] str::Utf8Error),
+}
+impl From<StrError> for io::Error {
+    fn from(err: StrError) -> Self {
+        io::Error::new(io::ErrorKind::InvalidData, err)
+    }
+}
 
 macro_attr! {
     #[derive(Clone, Debug, PartialEq, Eq, NewtypeDeref!, NewtypeDerefMut!)]
     pub struct BString(String);
 }
 impl BString {
-    pub fn new<B: AsRef<[u8]>> (chars: B) -> Result<Self> {
-        from_utf8_io(chars)
+    pub fn new<B: AsRef<[u8]>> (chars: B) -> Result<Self, StrError> {
+        from_utf8(chars)
     }
 }
 impl TryFrom<Vec<u8>> for BString {
-    type Error = Error;
-    fn try_from(chars: Vec<u8>) -> Result<Self> {
-        from_utf8_io(chars)
+    type Error = StrError;
+    fn try_from(chars: Vec<u8>) -> Result<Self, StrError> {
+        from_utf8(chars)
     }
 }
 impl FromStr for BString {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self> {
+    type Err = StrError;
+    fn from_str(s: &str) -> Result<Self, StrError> {
         check_len(s, 255)?;
         Ok(Self(s.to_owned()))
     }
@@ -43,18 +52,19 @@ impl ToString for BString {
 }
 impl Readable for BString {
     type ReadableArgs = ();
-    fn read_here<R: Read + Seek>(mut reader: R, _: &()) -> Result<Self> {
+    fn read_here<R: Read + Seek>(mut reader: R, _: &()) -> io::Result<Self> {
         let length: u8 = read_struct(&mut reader)?;
         let mut chars: Vec<u8> = vec![0u8; length as usize];
         reader.read_exact(&mut chars)?;
-        Self::try_from(chars)
+        let s = Self::try_from(chars)?;
+        Ok(s)
     }
 }
 impl Writable for BString {
     fn size(&self) -> usize {
         self.0.len() + 2 // length byte + chars + null
     }
-    fn write_here<W: Write>(&self, mut out: W) -> Result<()> {
+    fn write_here<W: Write>(&self, mut out: W) -> io::Result<()> {
         (self.0.len() as u8).write_here(&mut out)?;
         write_many(self.0.bytes(), &mut out)?;
         (0 as u8).write_here(&mut out)
@@ -67,20 +77,20 @@ macro_attr! {
 }
 
 impl ZString {
-    pub fn new<B: AsRef<[u8]>> (chars: B) -> Result<Self> {
-        from_utf8_io(chars)
+    pub fn new<B: AsRef<[u8]>> (chars: B) -> Result<Self, StrError> {
+        from_utf8(chars)
     }
 }
 impl FromStr for ZString {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self> {
+    type Err = StrError;
+    fn from_str(s: &str) -> Result<Self, StrError> {
         Ok(Self(s.to_owned()))
     }
 }
 impl TryFrom<Vec<u8>> for ZString {
-    type Error = Error;
-    fn try_from(chars: Vec<u8>) -> Result<Self> {
-        from_utf8_io(chars)
+    type Error = StrError;
+    fn try_from(chars: Vec<u8>) -> Result<Self, StrError> {
+        from_utf8(chars)
     }
 }
 
@@ -90,7 +100,7 @@ impl ToString for ZString {
     }
 }
 impl Readable for ZString {
-    fn read_here<R: Read + Seek>(mut reader: R, _: &()) -> Result<Self> {
+    fn read_here<R: Read + Seek>(mut reader: R, _: &()) -> io::Result<Self> {
         let mut chars: Vec<u8> = Vec::with_capacity(32);
         loop {
             let c: u8 = read_struct(&mut reader)?;
@@ -99,14 +109,15 @@ impl Readable for ZString {
             }
             chars.push(c);
         }
-        Self::try_from(chars)
+        let s = Self::try_from(chars)?;
+        Ok(s)
     }
 }
 impl Writable for ZString {
     fn size(&self) -> usize {
         self.0.len() + 1
     }
-    fn write_here<W: Write>(&self, mut writer: W) -> Result<()> {
+    fn write_here<W: Write>(&self, mut writer: W) -> io::Result<()> {
         write_many(self.0.bytes(), &mut writer)?;
         (0 as u8).write_here(writer)
     }
@@ -119,19 +130,19 @@ macro_attr! {
 }
 
 impl BZString {
-    pub fn new<B: AsRef<[u8]>> (chars: B) -> Result<Self> {
-        from_utf8_io(chars)
+    pub fn new<B: AsRef<[u8]>> (chars: B) -> Result<Self, StrError> {
+        from_utf8(chars)
     }
 }
 impl TryFrom<Vec<u8>> for BZString {
-    type Error = Error;
-    fn try_from(chars: Vec<u8>) -> Result<Self> {
-        from_utf8_io(chars)
+    type Error = StrError;
+    fn try_from(chars: Vec<u8>) -> Result<Self, StrError> {
+        from_utf8(chars)
     }
 }
 impl FromStr for BZString {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self> {
+    type Err = StrError;
+    fn from_str(s: &str) -> Result<Self, StrError> {
         check_len(s, 254)?;
         Ok(Self(s.to_owned()))
     }
@@ -144,35 +155,34 @@ impl ToString for BZString {
 }
 impl Readable for BZString {
     type ReadableArgs = ();
-    fn read_here<R: Read + Seek>(mut reader: R, _: &()) -> Result<Self> {
+    fn read_here<R: Read + Seek>(mut reader: R, _: &()) -> io::Result<Self> {
         let length: u8 = read_struct(&mut reader)?;
         let mut chars: Vec<u8> = vec![0u8; (length - 1) as usize]; // length field includes null.
         reader.read_exact(&mut chars)?;
         reader.seek(SeekFrom::Current(1))?; // skip null byte.
-        Self::try_from(chars)
+        let s = Self::try_from(chars)?;
+        Ok(s)
     }
 }
 impl Writable for BZString {
     fn size(&self) -> usize {
         self.0.len() + 2 // length byte + chars + null
     }
-    fn write_here<W: Write>(&self, mut out: W) -> Result<()> {
+    fn write_here<W: Write>(&self, mut out: W) -> io::Result<()> {
         (self.0.len() as u8 + 1).write_here(&mut out)?;
         write_many(self.0.bytes(), &mut out)?;
         (0 as u8).write_here(&mut out)
     }
 }
 
-fn from_utf8_io<B: AsRef<[u8]>, S: FromStr<Err = Error>>(chars: B) -> Result<S> {
-    match str::from_utf8(chars.as_ref()) {
-        Ok(s) => S::from_str(s),
-        Err(e) => Err(Error::new(ErrorKind::InvalidData, e)),
-    }   
+fn from_utf8<B: AsRef<[u8]>, S: FromStr<Err = StrError>>(chars: B) -> Result<S, StrError> {
+    let s = str::from_utf8(chars.as_ref())?;
+    S::from_str(s)
 }
 
-fn check_len(s: &str, max_len: usize) -> Result<()> {
+fn check_len(s: &str, max_len: usize) -> Result<(), StrError> {
     if s.len() > max_len {
-        Err(Error::new(ErrorKind::InvalidData, StringToLong(max_len)))
+        Err(StrError::TooLong(max_len))
     } else {
         Ok(())
     }
@@ -217,19 +227,19 @@ mod tests {
 
     #[test]
     fn bstring_len_check() {
-        len_check::<BString>();
+        len_check::<BString>(255);
     }
 
     #[test]
     fn bzstring_len_check() {
-        len_check::<BZString>();
+        len_check::<BZString>(254);
     }
 
-    fn len_check<S: FromStr<Err = Error> + Debug>() {
+    fn len_check<S: FromStr<Err = StrError> + Debug>(max_len: usize) {
         let s: String = (0..500).map(|_| 'a').collect();
         match S::from_str(&s) {
-            Ok(s)  => panic!("expected error but got error but got {:?}", s),
-            Err(_) => {},
+            Err(StrError::TooLong(l)) => assert_eq!(max_len, l, "max_len"),
+            res  => panic!("expected String too long error but got error but got {:?}", res),
         };
     }
 
