a47aae08c0a1a5c8d97615fadbf2e5215bef8452
Philipp Dargel <chisui.pd@gmail.com>
2021-10-02 18:10:35 +0200

    more tests for v105

--- bsa/src/v105.rs
+++ bsa/src/v105.rs
@@ -7,7 +7,7 @@ pub use super::bin::{read_struct, write_struct, Readable, Writable};
 pub use super::archive::{Bsa};
 pub use super::version::{Version, Version10X};
 pub use super::hash::{hash_v10x, Hash};
-pub use super::v10x::{V10XArchive, V10XWriter, V10XWriterOptions, Versioned};
+pub use super::v10x::{V10XArchive, V10XWriter, V10XWriterOptions, Versioned, DirContentRecord};
 pub use super::v10x;
 pub use super::v104::{ArchiveFlag, Header, BZString};
 
@@ -79,7 +79,8 @@ pub type BsaWriterOptions = V10XWriterOptions<ArchiveFlag>;
 
 #[cfg(test)]
 mod tests {
-    use std::io::Cursor;
+    use std::mem::size_of;
+    use std::io::{Cursor, SeekFrom};
     use enumflags2::BitFlags;
     use crate::archive::{FileId, BsaWriter, Bsa, BsaDirSource, BsaFileSource};
     use crate::version::{Version, Version10X};
@@ -88,28 +89,64 @@ mod tests {
 
     #[test]
     fn writes_version() {
-        let mut out = Cursor::new(Vec::<u8>::new());
-        v105::BsaWriter::write_bsa(BsaWriterOptions::default(), some_bsa_dirs(), &mut out)
-            .unwrap_or_else(|err| panic!("could not write bsa {}", err));
-        let mut bytes = Cursor::new(out.into_inner());
+        let mut bytes = some_bsa_bytes();
 
         let v = Version::read0(&mut bytes)
             .unwrap_or_else(|err| panic!("could not read version {}", err));
         assert_eq!(v, Version::V10X(Version10X::V105));
+    }
+
+    #[test]
+    fn writes_header() {
+        let mut bytes = some_bsa_bytes();
+
         let header = v105::Header::read0(&mut bytes)
             .unwrap_or_else(|err| panic!("could not read header {}", err));
 
+        assert_eq!(header.offset, 36, "offset");
         assert_eq!(header.archive_flags, BitFlags::empty()
             | v105::ArchiveFlag::IncludeFileNames
             | v105::ArchiveFlag::IncludeDirectoryNames);
+        assert_eq!(header.dir_count, 1, "dir_count");
+        assert_eq!(header.file_count, 1, "file_count");
+        assert_eq!(header.total_dir_name_length, 2, "total_dir_name_length");
+        assert_eq!(header.total_file_name_length, 2, "total_file_name_length");
+        assert_eq!(header.file_flags, BitFlags::empty(), "file_flags");
+    }
+
+    #[test]
+    fn writes_dir_records() {
+        let mut bytes = some_bsa_bytes();
+
+        v105::Header::read0(&mut bytes)
+            .unwrap_or_else(|err| panic!("could not read header {}", err));
+            
+        let dirs = RawDirRecord::read_many0(&mut bytes, 1)
+            .unwrap_or_else(|err| panic!("could not read dir records {}", err));
+
+        assert_eq!(dirs.len(), 1, "dirs.len()");
+        assert_eq!(dirs[0].file_count, 1, "dirs[0].file_count");
+    }
+
+    #[test]
+    fn writes_dir_content_records() {
+        let mut bytes = some_bsa_bytes();
+
+        bytes.seek(SeekFrom::Start(59))
+            .unwrap_or_else(|err| panic!("could not seek {}", err));
+            
+        let dir_content = v105::DirContentRecord::read_here0(&mut bytes)
+            .unwrap_or_else(|err| panic!("could not read dir content record {}", err));
+
+        assert_eq!(dir_content.name, Some(BZString::new("a").unwrap()), "dir_content.name");
+        assert_eq!(dir_content.files.len(), 1, "dir_content.files");
+        assert_eq!(dir_content.files[0].name_hash, hash_v10x("b"), "dir_content.files[0].name_hash");
+        assert_eq!(dir_content.files[0].size, 4, "dir_content.files[0].size");
     }
 
     #[test]
     fn write_read_identity() {
-        let mut out = Cursor::new(Vec::<u8>::new());
-        v105::BsaWriter::write_bsa(BsaWriterOptions::default(), some_bsa_dirs(), &mut out)
-            .unwrap_or_else(|err| panic!("could not write bsa {}", err));
-        let bytes = Cursor::new(out.into_inner());
+        let bytes = some_bsa_bytes();
         let mut bsa = v105::BsaArchive::open(bytes)
             .unwrap_or_else(|err| panic!("could not open bsa {}", err));
         let in_dirs = bsa.read_dirs()
@@ -125,8 +162,15 @@ mod tests {
     fn some_bsa_dirs() -> Vec<BsaDirSource<Vec<u8>>> {
         vec![
             BsaDirSource::new("a".to_owned(), vec![
-                    BsaFileSource::new("b".to_owned(), vec![])
+                    BsaFileSource::new("b".to_owned(), vec![0,0,0,0])
             ])
         ]
+    }
+
+    fn some_bsa_bytes() -> Cursor<Vec<u8>> {
+        let mut out = Cursor::new(Vec::<u8>::new());
+        v105::BsaWriter::write_bsa(BsaWriterOptions::default(), some_bsa_dirs(), &mut out)
+            .unwrap_or_else(|err| panic!("could not write bsa {}", err));
+        Cursor::new(out.into_inner())
     }
 }
