f8c338bf74dfcdaadd4be4a7ff96af8cdfb3ee62
Philipp Dargel <chisui.pd@gmail.com>
2021-11-12 23:20:41 +0100

    add more RecordTypes

--- esp/src/typed.rs
+++ esp/src/typed.rs
@@ -142,13 +142,13 @@ where R: Read + Seek {
                     }
                 },
                 raw::Entry::Group(g) => {
-                    let t = GroupType::try_from_primitive(g.group_info.group_type)
-                        .map_err(|err| EspError::Group(entry_path.clone(), GroupError::GroupUnknown(err)))?;
-                    if t == GroupType::TopLevel {
-                        let tlg = TopLevelGroup::read_group(&mut reader, &g)?;
+                    let info = GroupInfo::try_from(&g.group_info)
+                        .map_err(|err| EspError::Group(entry_path.clone(), GroupError::BadGroupInfo(err)))?;
+                    if let GroupInfo::TopLevel(rec) = info {
+                        let tlg = TopLevelGroup::read_group(&mut reader, rec, &g)?;
                         groups.push(tlg);
                     } else {
-                        return Err(EspError::Group(entry_path.clone(), GroupError::GroupUnexpected(t)));
+                        return Err(EspError::Group(entry_path.clone(), GroupError::GroupUnexpected(info.group_type())));
                     }
                 },
             }
@@ -242,6 +242,26 @@ pub enum GroupInfo {
         child_type: CellChildType,
     },
     Topic(FormId),
+}
+impl GroupInfo {
+    pub fn group_type(&self) -> GroupType {
+        match self {
+            GroupInfo::TopLevel(_) => GroupType::TopLevel,
+            GroupInfo::World(_) => GroupType::WorldChildren,
+            GroupInfo::CellBlock { block_type, info } => match (block_type, info) {
+                (CellBlockType::Block, BlockInfo::Interior(_)) => GroupType::CellBlockInterior,
+                (CellBlockType::Block, BlockInfo::Exterior(_)) => GroupType::CellBlockExterior,
+                (CellBlockType::SubBlock, BlockInfo::Interior(_)) => GroupType::CellSubBlockInterior,
+                (CellBlockType::SubBlock, BlockInfo::Exterior(_)) => GroupType::CellSubBlockExterior,
+            },
+            GroupInfo::CellChildren { child_type, .. } => match child_type {
+                CellChildType::Normal => GroupType::CellChildren,
+                CellChildType::Persistent => GroupType::CellPersistentChilden,
+                CellChildType::Temporary => GroupType::CellTemporaryChildren,
+            },
+            GroupInfo::Topic(_) => GroupType::TopicChildren,
+        }
+    }    
 }
 impl fmt::Display for GroupInfo {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -375,9 +395,9 @@ pub struct TopLevelGroup {
     pub record_type: RecordType,
 }
 impl TopLevelGroup {
-    fn read_group<R: Read + Seek>(reader: &mut raw::EspReader<R>, group: &raw::Group) -> EspRes<Self> {
+    fn read_group<R: Read + Seek>(reader: &mut raw::EspReader<R>, record_type: RecordType, group: &raw::Group) -> EspRes<Self> {
         Ok(Self {
-            record_type: RecordType::TES4,
+            record_type,
         })
     }
 }
@@ -388,10 +408,250 @@ pub struct ObjectId(u32);
 
 #[derive(
     Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord,
-    strum::IntoStaticStr, strum::EnumString, strum::Display
+    strum::IntoStaticStr, strum::EnumString, strum::Display, strum::EnumMessage
 )]
 pub enum RecordType {
+    AACT,
+    #[strum(message = "Actor Reference")]
+    ACHR,
+    #[strum(message = "Activator")]
+    ACTI,
+    #[strum(message = "Addon Node")]
+    ADDN,
+    #[strum(message = "Potion")]
+    ALCH,
+    #[strum(message = "Ammo")]
+    AMMO,
+    #[strum(message = "Animation Object")]
+    ANIO,
+    #[strum(message = "Apparatus ''(probably unused)''")]
+    APPA,
+    #[strum(message = "Armor Addon (Model)")]
+    ARMA,
+    #[strum(message = "Armor")]
+    ARMO,
+    #[strum(message = "Art Object")]
+    ARTO,
+    #[strum(message = "Acoustic Space")]
+    ASPC,
+    #[strum(message = "Association Type")]
+    ASTP,
+    #[strum(message = "Actor Values/Perk Tree Graphics")]
+    AVIF,
+    #[strum(message = "Book")]
+    BOOK,
+    #[strum(message = "Body Part Data")]
+    BPTD,
+    #[strum(message = "Camera Shot")]
+    CAMS,
+    #[strum(message = "Cell")]
+    CELL,
+    #[strum(message = "Class")]
+    CLAS,
+    #[strum(message = "Color")]
+    CLFM,
+    #[strum(message = "Climate")]
+    CLMT,
+    #[strum(message = "Constructible Object (recipes)")]
+    COBJ,
+    #[strum(message = "Collision Layer")]
+    COLL,
+    #[strum(message = "Container")]
+    CONT,
+    #[strum(message = "Camera Path")]
+    CPTH,
+    #[strum(message = "Combat Style")]
+    CSTY,
+    #[strum(message = "Debris")]
+    DEBR,
+    #[strum(message = "Dialog Topic")]
+    DIAL,
+    #[strum(message = "Dialog Branch")]
+    DLBR,
+    #[strum(message = "Dialog View")]
+    DLVW,
+    #[strum(message = "Default Object Manager")]
+    DOBJ,
+    #[strum(message = "Door")]
+    DOOR,
+    #[strum(message = "Dual Cast Data (possibly unused)")]
+    DUAL,
+    #[strum(message = "Encounter Zone")]
+    ECZN,
+    #[strum(message = "Effect Shader")]
+    EFSH,
+    #[strum(message = "Enchantment")]
+    ENCH,
+    #[strum(message = "Equip Slot (flag-type values)")]
+    EQUP,
+    #[strum(message = "Explosion")]
+    EXPL,
+    #[strum(message = "Eyes")]
+    EYES,
+    #[strum(message = "Faction")]
+    FACT,
+    #[strum(message = "Flora")]
+    FLOR,
+    #[strum(message = "Form List (non-leveled list)")]
+    FLST,
+    #[strum(message = "Footstep")]
+    FSTP,
+    #[strum(message = "Footstep Set")]
+    FSTS,
+    #[strum(message = "Furniture")]
+    FURN,
+    #[strum(message = "Global Variable")]
+    GLOB,
+    #[strum(message = "Game Setting")]
+    GMST,
+    #[strum(message = "Grass")]
+    GRAS,
+    #[strum(message = "Form Group")]
+    GRUP,
+    #[strum(message = "Hazard")]
+    HAZD,
+    #[strum(message = "Head Part")]
+    HDPT,
+    #[strum(message = "Idle Animation")]
+    IDLE,
+    #[strum(message = "Idle Marker")]
+    IDLM,
+    #[strum(message = "Image Space Modifier")]
+    IMAD,
+    #[strum(message = "Image Space")]
+    IMGS,
+    #[strum(message = "Dialog Topic Info")]
+    INFO,
+    #[strum(message = "Ingredient")]
+    INGR,
+    #[strum(message = "Impact Data")]
+    IPCT,
+    #[strum(message = "Impact Data Set")]
+    IPDS,
+    #[strum(message = "Key")]
+    KEYM,
+    #[strum(message = "Keyword")]
+    KYWD,
+    #[strum(message = "Landscape")]
+    LAND,
+    #[strum(message = "Location Reference Type")]
+    LCRT,
+    #[strum(message = "Location")]
+    LCTN,
+    #[strum(message = "Lighting Template")]
+    LGTM,
+    #[strum(message = "Light")]
+    LIGH,
+    #[strum(message = "Load Screen")]
+    LSCR,
+    #[strum(message = "Land Texture")]
+    LTEX,
+    #[strum(message = "Leveled Item")]
+    LVLI,
+    #[strum(message = "Leveled Actor")]
+    LVLN,
+    #[strum(message = "Leveled Spell")]
+    LVSP,
+    #[strum(message = "Material Object")]
+    MATO,
+    #[strum(message = "Material Type")]
+    MATT,
+    #[strum(message = "Message")]
+    MESG,
+    #[strum(message = "Magic Effect")]
+    MGEF,
+    #[strum(message = "Misc. Object")]
+    MISC,
+    #[strum(message = "Movement Type")]
+    MOVT,
+    #[strum(message = "Movable Static")]
+    MSTT,
+    #[strum(message = "Music Type")]
+    MUSC,
+    #[strum(message = "Music Track")]
+    MUST,
+    #[strum(message = "Navigation (master data)")]
+    NAVI,
+    #[strum(message = "NavMesh")]
+    NAVM,
+    #[strum(message = "Note")]
+    NOTE,
+    #[strum(message = "Actor (NPC, Creature)")]
+    NPC_,
+    #[strum(message = "Outfit")]
+    OTFT,
+    #[strum(message = "AI Package")]
+    PACK,
+    #[strum(message = "Perk")]
+    PERK,
+    #[strum(message = "Placed grenade")]
+    PGRE,
+    #[strum(message = "Placed hazard")]
+    PHZD,
+    #[strum(message = "Projectile")]
+    PROJ,
+    #[strum(message = "Quest")]
+    QUST,
+    #[strum(message = "Race / Creature type")]
+    RACE,
+    #[strum(message = "Object Reference")]
+    REFR,
+    #[strum(message = "Region (Audio/Weather)")]
+    REGN,
+    #[strum(message = "Relationship")]
+    RELA,
+    #[strum(message = "Reverb Parameters")]
+    REVB,
+    #[strum(message = "Visual Effect")]
+    RFCT,
+    #[strum(message = "Scene")]
+    SCEN,
+    #[strum(message = "Scroll")]
+    SCRL,
+    #[strum(message = "Shout")]
+    SHOU,
+    #[strum(message = "Soul Gem")]
+    SLGM,
+    #[strum(message = "Story Manager Branch Node")]
+    SMBN,
+    #[strum(message = "Story Manager Event Node")]
+    SMEN,
+    #[strum(message = "Story Manager Quest Node")]
+    SMQN,
+    #[strum(message = "Sound Category")]
+    SNCT,
+    #[strum(message = "Sound Reference")]
+    SNDR,
+    #[strum(message = "Sound Output Model")]
+    SOPM,
+    #[strum(message = "Sound")]
+    SOUN,
+    #[strum(message = "Spell")]
+    SPEL,
+    #[strum(message = "Shader Particle Geometry")]
+    SPGD,
+    #[strum(message = "Static")]
+    STAT,
+    #[strum(message = "Talking Activator")]
+    TACT,
+    #[strum(message = "Plugin info / Header")]
     TES4,
+    #[strum(message = "Tree")]
+    TREE,
+    #[strum(message = "Texture Set")]
+    TXST,
+    #[strum(message = "Voice Type")]
+    VTYP,
+    #[strum(message = "Water Type")]
+    WATR,
+    #[strum(message = "Weapon")]
+    WEAP,
+    #[strum(message = "Word Of Power")]
+    WOOP,
+    #[strum(message = "Worldspace")]
+    WRLD,
+    #[strum(message = "Weather")]
+    WTHR,
 }
 
 #[derive(Debug, Error)]
@@ -399,15 +659,15 @@ pub enum RecordTypeError {
     #[error("Record type is not a string: {0}")]
     NotString(raw::Label),
     #[error("Can't parse record type {0}")]
-    Malformed(#[from] strum::ParseError),
+    Malformed(raw::Label, strum::ParseError),
 }
 impl TryFrom<raw::Label> for RecordType {
     type Error = RecordTypeError;
 
     fn try_from(l: raw::Label) -> Result<Self, Self::Error> {
         if let Some(s) = l.as_str() {
-            let r = RecordType::from_str(s)?;
-            Ok(r)
+            RecordType::from_str(s)
+                .map_err(|err| RecordTypeError::Malformed(l, err))
         } else {
             Err(RecordTypeError::NotString(l))
         }
@@ -526,6 +786,8 @@ pub(crate) mod test {
     use std::fs::File;
     use std::io::Result;
 
+    use strum::EnumMessage;
+
     use super::*;
 
     #[test]
@@ -533,10 +795,16 @@ pub(crate) mod test {
         let f = File::open("../test-data/unofficialSkyrimSEpatch.esp")?;
         match EspFile::read(f) {
             Ok(f) => {
-                println!("{:#?}", f.header());
+                println!("{:?}", f.header().author);
+                for group in f.top_level_groups() {
+                    println!("{:?}: {}", group.record_type, group.record_type.get_message().unwrap());
+                }
                 Ok(())
             },
-            Err(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
+            Err(e) => {
+                println!("ERROR: {}", e);
+                Err(io::Error::new(io::ErrorKind::Other, e))
+            },
         }
     }
 }
