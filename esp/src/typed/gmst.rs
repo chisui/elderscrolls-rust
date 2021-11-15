ca184669295d2df7d6be01a0ae44cf1301a609a7
Philipp Dargel <chisui.pd@gmail.com>
2021-11-15 08:58:38 +0100

    refactored GMST to standard form

--- esp/src/typed/gmst.rs
+++ esp/src/typed/gmst.rs
@@ -1,3 +1,4 @@
+use std::convert::TryFrom;
 use std::io::{Read, Seek};
 
 use crate::raw;
@@ -13,49 +14,49 @@ pub struct GMST {
     pub key: EditorID,
     pub value: TypedValue,
 }
+impl TryFrom<PartGMST> for GMST {
+    type Error = RecordError;
+    fn try_from(tmp: PartGMST) -> Result<Self, Self::Error> {
+        let key = unwarp_field(tmp.edid, b"EDID")?;
+        let c = key.0.chars().next().ok_or_else(|| RecordError::Field(raw::Label(*b"EDID"), FieldError::Unexpectedize{
+            actual: 0,
+            expected: 1,
+        }))?;
+        let data = unwarp_field(tmp.data, b"DATA")?;
+        let value = TypedValue::new(c, data)
+            .map_err(|err| RecordError::Field(raw::Label(*b"DATA"), FieldError::Cast(err)))?;
+
+        Ok(Self { key, value })
+    }
+}
+#[derive(Debug, Clone, Default)]
+struct PartGMST {
+    edid: Option<EditorID>,
+    data: Option<Vec<u8>>,
+}
 impl GMST {
-    fn handle_field<R: Read + Seek>(reader: &mut raw::EspReader<R>, field: &raw::Field,
-            key: Option<EditorID>, value: Option<Vec<u8>>) -> Result<(Option<EditorID>, Option<Vec<u8>>), FieldError> {
+    fn handle_field<R: Read + Seek>(reader: &mut raw::EspReader<R>, field: &raw::Field, tmp: &mut PartGMST) -> Result<(), FieldError> {
         
         match field.field_type.as_str() {
-            Some("EDID") => {
-                if key.is_some() {
-                    Err(FieldError::Duplicate)
-                } else {
-                    let data = reader.content(&field)?;
-                    Ok((Some(data), value))
-                }
-            },
-            Some("DATA") => {
-                let data = reader.content(&field)?;
-                Ok((key, Some(data)))
-            },
-            _ => Err(FieldError::Unexpected)?,
+            Some("EDID") => tmp.edid = Some(reader.content(&field)?),
+            Some("DATA") => tmp.data = Some(reader.content(&field)?),
+            _ => return Err(FieldError::Unexpected),
         }
+        Ok(())
     }
 }
 impl Record for GMST {
     fn record_type(&self) -> RecordType {
         RecordType::GMST
     }
-
     fn read_rec<R: Read + Seek>(reader: &mut raw::EspReader<R>, rec: raw::Record) -> Result<Self, RecordError> {
-        let mut tmp = (None, None);
+        let mut tmp = PartGMST::default();
         
         for field in reader.fields(&rec)? {
-            tmp = GMST::handle_field(reader, &field, tmp.0, tmp.1)
+            Self::handle_field(reader, &field, &mut tmp)
                 .map_err(|err| RecordError::Field(field.field_type, err))?;
         }
 
-        let key = unwarp_field(tmp.0, b"EDID")?;
-        let c = key.0.chars().next().ok_or_else(|| RecordError::Field(raw::Label(*b"EDID"), FieldError::Unexpectedize{
-            actual: 0,
-            expected: 1,
-        }))?;
-        let data = unwarp_field(tmp.1, b"DATA")?;
-        let value = TypedValue::new(c, data)
-            .map_err(|err| RecordError::Field(raw::Label(*b"DATA"), FieldError::Cast(err)))?;
-
-        Ok(Self { key, value })
+        Self::try_from(tmp)
     }
 }
